6932925e7ab88ca6bb2ef34122362f785802875e
kiran68 <kiranurshil88@gmail.com>
2023-03-20 20:29:38 +0530

    Add: Check Vowel Or Constanant

--- BasicCoreProgram.java
+++ BasicCoreProgram.java
@@ -6,16 +6,13 @@ public class BasicCoreProgram {
 public static void main(String[] args) {
 	System.out.println("Welcome To Java Were We Learn Something Diffrent");
 	
-			Scanner reader = new Scanner(System.in);
-			System.out.println("Enter a number : ");
-			int num = reader.nextInt();
-			
-			if (num % 2 == 0)
-				
-			System.out.println(num + "is even: ");
-			else
-			System.out.println(num + "is odd: ");
-		
+	 System.out.println("Enter a character :");
+     Scanner check = new Scanner(System.in);
+     char ch = check.next().charAt(0);
+     if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
+        System.out.println(" character is an vowel");
+     }else{
+        System.out.println("character is a consonant");
 }
 }
-
+}
