3469024c3766867f2dfd04a488f65fed70a87202
kiran68 <kiranurshil88@gmail.com>
2023-03-20 20:38:56 +0530

    Add: Check  Largest Three Number

--- BasicCoreProgram.java
+++ BasicCoreProgram.java
@@ -6,13 +6,15 @@ public class BasicCoreProgram {
 public static void main(String[] args) {
 	System.out.println("Welcome To Java Were We Learn Something Diffrent");
 	
-	 System.out.println("Enter a character :");
-     Scanner check = new Scanner(System.in);
-     char ch = check.next().charAt(0);
-     if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
-        System.out.println(" character is an vowel");
-     }else{
-        System.out.println("character is a consonant");
-}
+	   int num1 = 6, num2 = 8, num3 = 5;
+
+       if( num1 >= num2 && num1 >= num3)
+           System.out.println(num1 + " is the largest number.");
+
+       else if (num2 >= num1 && num2 >= num3)
+           System.out.println(num2 + " is the largest number.");
+
+       else
+           System.out.println(num3 + " is the largest number.");
 }
 }
