f00a0eecbf59ddca7c3b47eb90c4c4e35fdef1fc
kiran68 <kiranurshil88@gmail.com>
2023-03-20 19:52:08 +0530

    Add: Compute Quatient And Remainder

---
+++ BasicCoreProgram.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.basiccoreProblem;
+
+public class BasicCoreProgram {
+public static void main(String[] args) {
+	System.out.println("Welcome To Java Were We Learn Something Diffrent");
+	
+	
+
+	    int divide = 50;
+	    int multi = 4;
+
+	    int quotient = divide / multi;
+	    int remainder = divide % multi;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	  }
+	}
+
+
