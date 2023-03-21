ac1207ba18c246edab0fbe26f9e225a66208a0ba
kiran68 <kiranurshil88@gmail.com>
2023-03-21 21:10:56 +0530

    Add: Factiration Number

--- BasicCoreProgram.java
+++ BasicCoreProgram.java
@@ -1,18 +1,27 @@
 package com.bridgelabz.basiccoreProblem;
 import java.util.Scanner;
+
 public class BasicCoreProgram {
-	public static void main(String[] args) {
+	public static void main(String[]args) {
 		System.out.println("Welcome To java Were We Learn Something Diffrent");
-		
-		double n,i;
-		double sum=0;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number");
-		n=sc.nextDouble();
-		for( i=1;i<=n;i++)
-		{
-			sum=sum+(1/i);
-		}
-		System.out.println("Harmonic value = "+sum);
+
+	Scanner input = new Scanner (System.in);
+  System.out.println("Enter a number to find its prime Factors ");
+  int number = input.nextInt();
+  
+  System.out.println("Prime Dactors of " + number + " are ");
+  
+  
+ for (int i = 2; i*i<number;i++) {
+	 while(number % i == 0) {
+		 System.out.println(i + "");
+		 number /=10;
+		 
+ }
 	}
+ if (number > 1)
+ {
+	 System.out.println(number);
+ }
+}
 }
