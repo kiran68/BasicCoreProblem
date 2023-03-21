0f565f988ba2abf0af632f2f7bfe6e35319e8c12
kiran68 <kiranurshil88@gmail.com>
2023-03-21 12:40:27 +0530

    Add: Flip Coint Head Or Tails

--- BasicCoreProgram.java
+++ BasicCoreProgram.java
@@ -1,20 +1,37 @@
 package com.bridgelabz.basiccoreProblem;
-
+import java.util.Scanner;
 import java.util.Scanner;
 
 public class BasicCoreProgram {
 public static void main(String[] args) {
 	System.out.println("Welcome To Java Were We Learn Something Diffrent");
+
+	int head = 0;
+	int tails = 0;
+	double random = 0.0;
+	int count = 1;
 	
-	   int num1 = 6, num2 = 8, num3 = 5;
+System.out.println("Please enter the number : ");
+Scanner num = new Scanner(System.in);
+int flips = num.nextInt();
 
-       if( num1 >= num2 && num1 >= num3)
-           System.out.println(num1 + " is the largest number.");
-
-       else if (num2 >= num1 && num2 >= num3)
-           System.out.println(num2 + " is the largest number.");
-
-       else
-           System.out.println(num3 + " is the largest number.");
+while (count <= flips) {
+	
+	random =Math.random();
+	System.out.println(count+" "+random);
+	
+	if (random < 0.5) {
+		head++;
+		System.out.println("Heads");
+	}
+		else {
+			tails++;
+			System.out.println("Tails");
+	}
+	count++;
+	}
 }
+
+
 }
+
